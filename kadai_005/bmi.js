d1fd4db0080973ecb5c82943ebb996207fa5ca99
yamadasatoru <75406327+yamadasatoru@users.noreply.github.com>
2024-03-24 22:51:12 +0900

    身長体重をそれぞれ変数を定義

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -2,4 +2,14 @@
 // 身長1.7m
 // 計算式：[体重(kg)]÷[身長(m)×身長(m)]
 
-console.log(68 / (1.7 * 1.7));
+//体重の変数
+let userWeight;
+
+userWeight = 68;
+
+//　身長の変数
+let userHeight;
+
+userHeight = 1.7;
+
+console.log(userWeight / (userHeight * userHeight));
