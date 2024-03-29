b106ac226904e420326d8374434c5f56c8f384bf
yamadasatoru <75406327+yamadasatoru@users.noreply.github.com>
2024-03-29 22:56:08 +0900

    3と5の倍数ですの条件分岐を一番上に。

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,19 +1,19 @@
 // 変数numに0～15までのランダムな整数を代入する
 let num = Math.floor(Math.random() * 16 );
 
-
-if (num % 3 === 0 ) {
-  console.log('3の倍数です');
-}
-
-else if (num % 5 === 0) {
-    console.log('5の倍数です');
-  }
-//
-else if (num % 3 === 0 && num % 5 === 0) {
+// 先に３と５の倍数の場合をチェックする
+if (num % 3 === 0 && num % 5 === 0) {
   console.log('3と５の倍数です');
 }
-
+// 次に3の倍数だけをチェックする
+else if (num % 3 === 0 ) {
+  console.log('3の倍数です');
+  }
+// その後で５の倍数だけをチェックする
+else if (num % 5 === 0) {
+  console.log('5の倍数です');
+}
+// それ以外の場合、numの値をそのまま出力する
 else {
   console.log(num);
 }
