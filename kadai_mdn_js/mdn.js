ebeffe953c30909f9fa803e24f6db6bf2d5f5bc6
yamadasatoru <75406327+yamadasatoru@users.noreply.github.com>
2024-03-30 10:13:04 +0900

    月は値が1ずれるため、プラス１を追加

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const date = new Date();
 const year = date.getFullYear() + '年';
-const month = date.getMonth() + '月';
+const month = date.getMonth()+1 + '月';
 const day = date.getDate() + '日';
 
 console.log(year + month + day);
