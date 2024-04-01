7b34f7b40e2451c1f96056502ead5e1709444143
yamadasatoru <75406327+yamadasatoru@users.noreply.github.com>
2024-04-01 22:36:25 +0900

    textも定数に代入する。

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,13 @@
 
+// btnというidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
+
+// textというidをもつHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
-  //下記でid textを指定。btnを指定するとボタンのテキストが変わる
+  //newTextに「ボタンをクリックしました」というテキストを追加する
   text.textContent = 'ボタンをクリックしました';
+  console.log(text);
 })
